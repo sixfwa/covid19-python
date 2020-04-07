7def5fbbb9494942866cf83817e1a17ee5ba05cf
sixfwa <farhanaliw6@gmail.com>
2020-04-07 10:23:20 +0100

    aggregate countries within population range

--- mongodb.py
+++ mongodb.py
@@ -67,7 +67,61 @@ def aggregate_average_countries(countries, feature):
     dates = []
     features = []
     for item in agg:
+        # .strftime("%d/%m/%y")
         dates.append(item["_id"].date())
         features.append(item["average_{}".format(feature)])
 
     return dates, features
+
+
+def aggregate_population_countries(greater_than, less_than, feature):
+    """
+    Compares the daily number of cases or deaths of countries within an population range
+    """
+    countries = {}
+    agg = collection.aggregate([
+        {
+            "$match": {
+                "$and": [{"popData2018": {"$lt": less_than, "$gt": greater_than}}]
+                # "popData2018": {"$and": [{"$lt": less_than}, {"$gt": greater_than}]}
+            }
+        },
+        {
+            "$project": {
+                "dateRep": {"$toDate": {"$concat": ["$year", "-", "$month", "-", "$day"]}},
+                "{}".format(feature): "${}".format(feature),
+                "countriesAndTerritories": "$countriesAndTerritories"
+            }
+        },
+        {
+            "$sort": {"dateRep": 1}
+        }
+    ])
+
+    for item in agg:
+        date = item["dateRep"].date()
+        country_name = item["countriesAndTerritories"]
+        # print("{} --> {}".format(date, country_name))
+        if country_name not in countries:
+            countries[country_name] = {}
+            countries[item["countriesAndTerritories"]][date] = item[feature]
+        else:
+            countries[item["countriesAndTerritories"]][date] = item[feature]
+
+    return countries
+
+
+def dates_cases_totals(groups, feature):
+    for group in groups:
+        dates, cases = aggregate_countries(group, feature)
+        for date, case in zip(dates, cases):
+            groups[group][date] = case
+    return groups
+
+
+def dates_cases_averages(groups, feature):
+    for group in groups:
+        dates, cases = aggregate_average_countries(group, feature)
+        for date, case in zip(dates, cases):
+            groups[group][date] = case
+    return groups
